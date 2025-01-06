9c8292dee0e2ebd64d50402feca0b303777c1c44
zeeshanshaikh7 <skzeeshan10011@gmail.com>
2025-01-06 16:53:53 +0530

    allJobsPostedByEmployer api added

--- src/routers/employer/employerController/createJobController.ts
+++ src/routers/employer/employerController/createJobController.ts
@@ -1,35 +1,10 @@
-import { RequestHandler } from "express";
+import { NextFunction, RequestHandler, Response } from "express";
 import Job, { IJob } from "../../job/models/Job";
 import EmployerProfile from "../models/EmployerProfile";
 import mongoose from "mongoose";
 import { validateJobInput } from "../../../utils/validateJobInputs";
-
-// export const createJobController: RequestHandler = async (
-//   req: any,
-//   res,
-//   next
-// ) => {
-//   const userId = req.user?.id;
-//   try {
-//     try {
-//       const jobData: IJob = req.body;
-//       jobData.company = userId;
-//       const newJob = new Job(jobData);
-//       await newJob.save();
-//       res
-//         .status(201)
-//         .json({ success: true, message: "Job created successfully" });
-//     } catch (error) {
-//       console.log(error);
-
-//       res.status(500).json({ message: "Error creating job", error });
-//     }
-//   } catch (error) {
-//     console.log(error);
-
-//     next(error);
-//   }
-// };
+import createHttpError from "http-errors";
+import AppliedJobs from "../../job/models/AppliedJobs.model";
 
 export const createJobController: RequestHandler = async (
   req: any,
@@ -39,8 +14,7 @@ export const createJobController: RequestHandler = async (
   const userId = req.user?.id;
 
   if (!userId) {
-    res.status(401).json({ success: false, message: "Unauthorized" });
-    return next();
+    return next(createHttpError(401, "Unauthorized"));
   }
 
   try {
@@ -48,23 +22,15 @@ export const createJobController: RequestHandler = async (
 
     const validationError = validateJobInput(jobData);
     if (validationError) {
-      res.status(400).json({ success: false, message: validationError });
-      return next();
+      return next(createHttpError(400, `${validationError}`));
     }
 
     const employerProfile = await EmployerProfile.findOne({ userId });
 
     if (employerProfile) {
-      jobData.company = employerProfile._id as any;
+      jobData.company = employerProfile._id as mongoose.Schema.Types.ObjectId;
     } else {
-      res
-        .status(404)
-        .json({ success: false, message: "Employer profile not found" });
-    }
-
-    // Convert deadline string to Date object if it's not already
-    if (typeof jobData.deadline === "string") {
-      jobData.deadline = new Date(jobData.deadline);
+      return next(createHttpError(404, "Employer profile not found"));
     }
 
     const newJob = new Job(jobData);
@@ -76,11 +42,75 @@ export const createJobController: RequestHandler = async (
       job: newJob,
     });
   } catch (error) {
-    console.error("Error creating job:", error);
-    res.status(500).json({
-      success: false,
-      message: "Error creating job",
-      error: error instanceof Error ? error.message : String(error),
+    return next(createHttpError(500, "Error while creating job"));
+  }
+};
+
+export const getMyJobsController: any = async (
+  req: any,
+  res: Response,
+  next: NextFunction
+) => {
+  const userId = req.user?.id;
+
+  if (!userId) {
+    return next(createHttpError(401, "Unauthorized: Missing user ID"));
+  }
+
+  try {
+    // Find the employer's profile based on the userId
+    const employerProfile = await EmployerProfile.findOne({ userId });
+    if (!employerProfile) {
+      return next(createHttpError(404, "Employer profile not found"));
+    }
+
+    // Fetch all jobs associated with the employer
+    const jobs = await Job.find({ company: employerProfile._id });
+
+    if (jobs.length === 0) {
+      return res.status(200).json({
+        success: true,
+        message: "No jobs found for this employer",
+        data: [],
+      });
+    }
+
+    const appliedUsersByJobId = await AppliedJobs.aggregate([
+      {
+        $match: {
+          jobId: { $in: jobs.map((job) => job._id) },
+        },
+      },
+      {
+        $unwind: "$jobId",
+      },
+      {
+        $lookup: {
+          from: "jobs",
+          localField: "jobId",
+          foreignField: "_id",
+          as: "jobDetails",
+        },
+      },
+      {
+        $unwind: "$jobDetails",
+      },
+      {
+        $group: {
+          _id: "$jobId",
+          appliedUsers: { $push: "$userId" },
+          jobDetails: { $first: "$jobDetails" },
+        },
+      },
+    ]);
+
+    res.status(200).json({
+      success: true,
+      message: "Jobs fetched successfully",
+      data: appliedUsersByJobId,
     });
+  } catch (error) {
+    console.error("Error fetching jobs:", error);
+    return next(createHttpError(500, "An error occurred while fetching jobs"));
   }
 };
--- src/routers/employer/employerRoutes.ts
+++ src/routers/employer/employerRoutes.ts
@@ -1,9 +1,18 @@
 import express from "express";
 import { updateProfileController } from "./employerController/updateUserProfile";
 import { authenticateToken } from "../../middlewares/authMiddleware";
-import { createJobController } from "./employerController/createJobController";
+import {
+  createJobController,
+  getMyJobsController,
+} from "./employerController/createJobController";
 const employerRouter = express.Router();
 // Route for user registration with validation middleware
-employerRouter.post("/updateProfile", authenticateToken, updateProfileController);
+employerRouter.post(
+  "/updateProfile",
+  authenticateToken,
+  updateProfileController
+);
+
 employerRouter.post("/createJob", authenticateToken, createJobController);
+employerRouter.get("/allJobsPostedByEmployer",authenticateToken, getMyJobsController);
 export default employerRouter;
