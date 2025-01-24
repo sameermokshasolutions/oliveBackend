5c64da8c1a946194205523e004d58366c3ed2c9a
sameer shaikh <sameer.shaikh@mokshasolutions.com>
2025-01-24 17:52:17 +0530

    feat(api): applicants api added

--- src/routers/employer/employerController/candidates.ts
+++ src/routers/employer/employerController/candidates.ts
@@ -20,19 +20,145 @@ export const getAppliedCandidates: RequestHandler = async (
 
     const userIds = documents.map((doc) => doc.userId);
 
-    const candidateData = await CandidateModel.find({
+    if (userIds.length === 0) {
+      res.status(200).json({
+        success: true,
+        message: "No candidate applied to this job",
+        data: null,
+      });
+    }
+    const jobData = await Job.findById(jobId).select("jobRole -_id");
+
+    const result = await CandidateModel.find({
       userId: { $in: userIds },
     }).populate({
       path: "userId",
       select: "firstName lastName -_id",
     });
 
+    const applicants: any = await CandidateModel.aggregate([
+      {
+        $match: {
+          userId: { $in: userIds },
+        },
+      },
+      {
+        $lookup: {
+          localField: "userId",
+          foreignField: "_id",
+          from: "users",
+          as: "candidateData",
+        },
+      },
+      {
+        $project: {
+          "candidateData.firstName": 1,
+          "candidateData.lastName": 1,
+          availability: 1,
+          awardsAndHonors: 1,
+          biography: 1,
+          gender: 1,
+          educationList: 1,
+          expectedSalary: 1,
+          experienceList: 1,
+          jobPreferences: 1,
+          jobRolePreferences: 1,
+          languages: 1,
+          maritalStatus: 1,
+          preferredJobLocation: 1,
+          relocationPreference: 1,
+          skills: 1,
+          socialList: 1,
+          profileUrl: 1,
+        },
+      },
+    ]);
+
     res.status(200).json({
       success: true,
       message: "Applications",
-      data: candidateData,
+      data: result,
     });
   } catch (error) {
-    return next(createHttpError(500, "Error while creating job"));
+    return next(createHttpError(500, "Error while getting applicants"));
   }
 };
+
+/**
+ 
+  try {
+    const { jobId } = req.params;
+
+    const documents = await AppliedJobsByCandidateModel.find({
+      "appliedJobs.jobId": jobId,
+    }).select("userId");
+
+    const userIds = documents.map((doc) => doc.userId);
+
+    if (userIds.length === 0) {
+      res.status(200).json({
+        success: true,
+        message: "No candidate applied to this job",
+        data: null,
+      });
+    }
+    const jobData = await Job.findById(jobId).select("jobRole -_id");
+
+    const applicants: any = await CandidateModel.aggregate([
+      {
+        $match: {
+          userId: { $in: userIds },
+        },
+      },
+      {
+        $lookup: {
+          localField: "userId",
+          foreignField: "_id",
+          from: "users",
+          as: "candidateData",
+        },
+      },
+      {
+        $addFields: {
+          firstName: "$candidateData.firstName",
+          lastName: "$candidateData.lastName",
+        },
+      },
+      {
+        $project: {
+          firstName: 1,
+          lastName: 1,
+          availability: 1,
+          awardsAndHonors: 1,
+          biography: 1,
+          gender: 1,
+          educationList: 1,
+          expectedSalary: 1,
+          experienceList: 1,
+          jobPreferences: 1,
+          jobRolePreferences: 1,
+          languages: 1,
+          maritalStatus: 1,
+          preferredJobLocation: 1,
+          relocationPreference: 1,
+          skills: 1,
+          socialList: 1,
+          profileUrl: 1,
+        },
+      },
+    ]);
+
+    const result = {
+      ...applicants[0],
+      ...jobData?.toObject(),
+    };
+
+    res.status(200).json({
+      success: true,
+      message: "Applications",
+      data: result,
+    });
+  } catch (error) {
+    return next(createHttpError(500, "Error while getting applicants"));
+  }
+ */
