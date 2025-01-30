c93213b5872a818094b99c2112033926eed4319b
sameer shaikh <sameer.shaikh@mokshasolutions.com>
2025-01-30 18:28:10 +0530

    lax

--- src/routers/user/userController/authController.ts
+++ src/routers/user/userController/authController.ts
@@ -60,15 +60,15 @@ export const loginUser = async (
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite:process.env.NODE_ENV === "production"? "none": "strict",
+      sameSite: "lax",
       maxAge: 10 * 60 * 60 * 1000,
     });
     res.cookie("userRole", existingUser.role, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite:process.env.NODE_ENV === "production"? "none": "strict",
+      sameSite: "lax",
       maxAge: 10 * 60 * 60 * 1000,
-    }); 
+    });
 
     res.status(200).json({
       success: true,
