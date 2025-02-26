36a14bb1be2959cf60db26ca00bf0f9d3f691833
sameer shaikh <sameer.shaikh@mokshasolutions.com>
2025-02-26 11:08:20 +0530

    z

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -3,11 +3,11 @@
 import { Request } from "express";
 
 declare global {
-  // namespace Express {
-  //     interface Request {
-  //         user?: { id: string };
-  //     }
-  // }
+  namespace Express {
+    interface Request {
+      user?: { id: string };
+    }
+  }
 }
 
 // Ensure the file is treated as a module
